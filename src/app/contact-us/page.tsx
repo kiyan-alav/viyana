467d059168df3a8d84dc869eb670bcdf45b1f67b
k.alavi <kiyanalavi1379@gmail.com>
2025-01-16 15:41:07 +0330

    - create contact us page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from "@/components/layouts/MainLayout";
+
+function ContactUs(props) {
+    return (
+        <MainLayout>
+            <div className="">555</div>
+        </MainLayout>
+    );
+}
+
+export default ContactUs;
